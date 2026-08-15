fe86b83d3ebb3117898c0393ba0c8c3717698ffa
vivekkushwahaofficial <185205704+vivekkushwahaofficial@users.noreply.github.com>
2026-08-16 03:35:08 +0530

    chore: initialize JavaScript learning repository

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 353645
+
+let accountEmail = "vivek@google.com"
+var accountPassword = "123424"
+accountCity = "Bhopal"
+let accountState
+
+account = 2 // not allowed
+
+accountEmail = "abs@google.com"
+accountPassword = "1242435"
+accountCity = "Indore"
+
+console.log(account)
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+/*
+Prefer not to use var
+because of issue in block scope and functional scope*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3 + 3)  //we are using nodejs, not browser
+
+console.log(4 + 3)
+
+console.log("Vivek")
+
+let name = "Vivek"
+let age = 22
+let isLiggingIn = false
+
+
+// number => 2 to power 53
+// bigint => bigger num
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => for finding uniqueness
+
+// object
+
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof ndefined); //undefined
+
