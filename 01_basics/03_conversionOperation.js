80dc3abecc90908748db07585588c06659855ff2
vivekkushwahaofficial <185205704+vivekkushwahaofficial@users.noreply.github.com>
2026-08-16 16:53:05 +0530

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+// let score = "33"
+// let score = 
+// let score = undefined
+// let score = true
+// let score = "Vivek"
+
+
+// console.log(typeof(score));
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/*
+"33" => 33
+true => 1, false => 0
+*/
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "Vivek"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+/*
+1 => true, 0 => false
+"" => false
+"Vivek" => true
+*/
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
