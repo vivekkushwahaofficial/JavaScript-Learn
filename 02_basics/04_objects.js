44ef0cefe1bae9850d7d2778e43bfeb9df5a7508
vivekkushwahaofficial <185205704+vivekkushwahaofficial@users.noreply.github.com>
2026-08-22 00:53:14 +0530

    feat: practice JavaScripts objects and apis

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -49,12 +49,23 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
 
+const course = {
+  coursename: "JS in hindi",
+  price: "999",
+  courseInstructor: "Hitesh Chaudhary"
+}
+
+// console.log(course.courseInstructor);
+
+const{courseInstructor: instructor} = course
+// console.log(courseInstructor);
+console.log(instructor);
 
